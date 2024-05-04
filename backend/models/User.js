b421e627da2d16fb74813949d2b749b1082d1b98
prathikshetty14 <prathikshetty1411@gmail.com>
2024-05-04 19:45:33 +0530

    Bug fix

---
+++ backend/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+
+
+//Create User Schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: function (email) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+      },
+      message: "Please enter a valid email."
+    }
+  },
+  password: {
+    type: String,
+    trim: true,
+    min: 6
+  },
+  // email: {
+  //   type: String,
+  //   required: true,
+  //   unique: true
+  // },
+  permissions: {
+    type: String
+  },
+});
+
+
+//Add passportLocalMongoose Plugin to hash and salt user password
+userSchema.plugin(passportLocalMongoose);
+
+//Add findOrCreate mongoose Plugin
+userSchema.plugin(findOrCreate);
+
+
+//Export User Model
+module.exports = mongoose.model("User", userSchema);
